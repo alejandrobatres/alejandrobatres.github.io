dfa909c464c3b6d39b3418d23cfe421bfd3ac51f
alejandrobatres <alejandrobatress@gmail.com>
2022-09-28 17:42:24 +0000

    Deploying to gh-pages from @ alejandrobatres/alejandrobatres.github.io@1c64b045641459ff11f26edaaa5b7d777914ba61 🚀

---
+++ _next/static/6E1PE1mfcCAIqo4jfs5qg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/570-d693e13327e928c0.js","static/chunks/pages/index-206244cd3a40c7b8.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
